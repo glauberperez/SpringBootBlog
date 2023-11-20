d5e1bb083cc058c80d31888db95601cf3236a46f
glauberperez <glauber2070@hotmail.com>
2023-11-19 22:47:43 -0300

    publishing but feed is blank

--- src/main/java/com/glauberperez/blog/service/UserService.java
+++ src/main/java/com/glauberperez/blog/service/UserService.java
@@ -1,10 +1,8 @@
 package com.glauberperez.blog.service;
 
 import com.glauberperez.blog.model.UserModel;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserService {
     List<UserModel> getAllUsers();
